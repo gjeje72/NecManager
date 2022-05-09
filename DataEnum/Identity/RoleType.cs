d70c6dfb89de8db1460e55729ed30530357c028c
Jey45 <gjeje72@hotmail.fr>
2022-05-09 21:30:57 +0200

    Add DataEnum

---
+++ DataEnum/Identity/RoleType.cs
@@ -0,0 +1,33 @@
+﻿namespace DataEnum.Identity;
+
+using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+///     All available roles in this application. 
+/// </summary>
+public enum RoleType
+{
+    /// <summary>
+    ///     Admin.
+    /// </summary>
+    [Display(ResourceType = typeof(RoleResource), Description = "ADMIN")]
+    ADMIN = 0,
+
+    /// <summary>
+    ///     DL SERVICES employee.
+    /// </summary>
+    [Display(ResourceType = typeof(RoleResource), Description = "EMPLOYEE")]
+    EMPLOYEE = 1,
+
+    /// <summary>
+    ///     User.
+    /// </summary>
+    [Display(ResourceType = typeof(RoleResource), Description = "USER")]
+    USER = 2,
+
+    /// <summary>
+    ///     Manager.
+    /// </summary>
+    [Display(ResourceType = typeof(RoleResource), Description = "MAN")]
+    MANAGER = 3,
+}
