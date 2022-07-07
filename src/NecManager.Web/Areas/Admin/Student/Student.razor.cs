af18cb1d693650e54be52c1d3cd8039d3aaf62e2
gjeje72 <gjeje72@hotmail.fr>
2022-07-07 11:04:10 +0200

    [US-6] front create student form

---
+++ src/NecManager.Web/Areas/Admin/Student/Student.razor.cs
@@ -0,0 +1,26 @@
+﻿namespace NecManager.Web.Areas.Admin.Student;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using NecManager.Web.Areas.Admin.Student.ViewModel;
+
+public partial class Student
+{
+    private bool creatingInProgress;
+
+    private CreateUserViewModel CreateUserModel { get; set; } = new();
+
+    private List<CreateUserViewModel> Users = new();
+    private void OpenCreatingForm()
+    {
+        this.creatingInProgress = !this.creatingInProgress;
+    }
+
+    private async Task HandleSubmitAsync()
+    {
+        this.Users.Add(this.CreateUserModel);
+        this.CreateUserModel = new();
+        await this.InvokeAsync(() => this.StateHasChanged());
+    }
+}
